33c966f8b9d5e15eb4ee3d40c3782d3520f1fe9e
dutiz <zaimidaut@gmail.com>
2023-06-29 20:17:47 +0200

    fixing bugs

--- components/sections/TeamSection.js
+++ components/sections/TeamSection.js
@@ -2,18 +2,20 @@ import TeamCart from '../TeamCart'
 
 export default function TeamSection() {
   return (
-    <div className="container">
-      <div className="flex flex-row justify-center">
-        <div className="col-6">
-          <h1 className="mt-20 text-center uppercase text-6xl font-bold border-l-8 border-green-500">
-            Staff Memebers
-          </h1>
+    <div className="bg-gray-200">
+      <div className="container">
+        <div className="flex flex-row justify-center">
+          <div className="col-6">
+            <h1 className="mt-20 text-center uppercase text-6xl font-bold border-l-8 border-green-500">
+              Staff Members
+            </h1>
+          </div>
         </div>
-      </div>
-      <div className="row mt-20">
-        <TeamCart img="/img/team-1.png" name="Enis Miftari" />
-        <TeamCart img="/img/team-1.png" name="Enis Miftari" />
-        <TeamCart img="/img/team-1.png" name="Enis Miftari" />
+        <div className="row my-20">
+          <TeamCart img="/img/team-1.png" name="Enis Miftari" />
+          <TeamCart img="/img/team-1.png" name="Enis Miftari" />
+          <TeamCart img="/img/team-1.png" name="Enis Miftari" />
+        </div>
       </div>
     </div>
   )
