2450297f48e6e07ae00d1b693c7a0199a3fc95a1
dutiz <zaimidaut@gmail.com>
2023-06-29 20:40:12 +0200

    navbar updated

--- components/layout/Navigation.js
+++ components/layout/Navigation.js
@@ -1,12 +1,13 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import { useState } from 'react'
 import SVG from 'react-inlinesvg'
 
-const NavItem = ({ children, href }) => {
+const NavItem = ({ className, children, href }) => {
   return (
     <li className="w-full md:w-auto md:mr-6">
       <Link
-        className="block py-4 md:py-0 border-b border-gray-200 md:border-b-0 md:border-transparent text-white text-lg font-bold uppercase"
+        className={`${className} block py-4 md:py-0 border-b border-gray-200 md:border-b-0 md:border-transparent  text-lg font-bold uppercase`}
         href={href}
       >
         {children}
@@ -18,20 +19,34 @@ const NavItem = ({ children, href }) => {
 export default function Navigation() {
   const [menu, setMenu] = useState(false)
 
+  const [color, setColor] = useState(false)
+  const changeColor = () => {
+    if (window.scrollY >= 90) {
+      setColor(true)
+    } else {
+      setColor(false)
+    }
+  }
+
+  window.addEventListener('scroll', changeColor)
+
   return (
-    <div className={`fixed w-full top-0 left-0`} style={{ zIndex: '9999' }}>
+    <div
+      className={`fixed w-full top-0 left-0 ${color ? 'bg-gray-200' : 'bg-transparent'}`}
+      style={{ zIndex: '9999' }}
+    >
       <div className="container">
         <div className="row py-6 justify-between items-center">
           <div className="col-4">
             <Link href="/">
-              <h1 className="text-white">Vindex</h1>
+              <Image src="/img/logo.png" width={220} height={123} alt="logo" />
             </Link>
           </div>
           <div className="col-2 md:hidden block">
             <label htmlFor="menu-toggle" className="cursor-pointer">
               <SVG
                 src="../svg/menu.svg"
-                className={`fill-current  w-6 h-6`}
+                className={`fill-current  ${color ? 'text-black' : 'text-black'} w-6 h-6`}
                 onClick={() => setMenu(!menu)}
               />
             </label>
@@ -43,9 +58,21 @@ export default function Navigation() {
             }`}
           >
             <ul className="flex w-full p-5 flex-col md:w-auto md:p-0 md:flex-row items-center">
-              <NavItem href="/">Gallery</NavItem>
-              <NavItem href="/about">Pricing</NavItem>
-              <NavItem href="/contact">Contact</NavItem>
+              <NavItem className={`${color ? 'text-black ' : 'text-white md:text-white'}`} href="/">
+                Gallery
+              </NavItem>
+              <NavItem
+                className={`${color ? 'text-black ' : 'text-white md:text-white'}`}
+                href="/about"
+              >
+                Pricing
+              </NavItem>
+              <NavItem
+                className={`${color ? 'text-black ' : 'text-white md:text-white'}`}
+                href="/contact"
+              >
+                Contact
+              </NavItem>
             </ul>
           </div>
         </div>
