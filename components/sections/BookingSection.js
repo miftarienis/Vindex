60cdd588471699f2e5af7f924c6aa8ac04169f8b
dutiz <zaimidaut@gmail.com>
2023-07-02 23:40:37 +0200

    calendar css modified

--- components/sections/BookingSection.js
+++ components/sections/BookingSection.js
@@ -1,20 +1,20 @@
-import 'react-calendar/dist/Calendar.css'
-
 import { useState } from 'react'
 import Calendar from 'react-calendar'
 
 export default function BookingSection() {
   const [date, setDate] = useState(new Date())
   return (
-    <div className="bg-gray-200">
+    <div className="bg-gray-200" id="calendar">
       <div className="container">
         <div className="row my-20">
-          <div className="col-8">
-            <h2 className="uppercase text-3xl font-bold">Choose a date</h2>
-            <Calendar onChange={setDate} value={date} className="mt-7 w-full" />
+          <div className="lg:col-8">
+            <h2 className="uppercase text-3xl lg:text-start text-center font-bold">
+              Choose a date
+            </h2>
+            <Calendar onChange={setDate} value={date} />
           </div>
-          <div className="col-4 text-center">
-            <h2 className="uppercase text-3xl font-bold text-start">Select time</h2>
+          <div className="lg:col-4 mt-5 text-center">
+            <h2 className="uppercase text-3xl font-bold text-center lg:text-start">Select time</h2>
             <input
               type="text"
               className="mt-4 w-full py-6 rounded-lg placeholder:text-gray-200 text-center uppercase"
