974bac1d518c585709b5e63bd78e80b4dc24df04
miftarienis <enismiftari2@outlook.com>
2023-09-10 22:59:11 +0200

    favicon added

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -12,6 +12,10 @@ export default function Layout({ children, title = null }) {
     <>
       <Head>
         <title>{title ? `${title} - Vindex` : 'Vindex Gym'}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+        <link rel="manifest" href="/favicons/site.webmanifest" />
       </Head>
       <div className={`font-sans antialiased flex flex-col ${dev ? 'debug-screens' : ''}`}>
         {/* Navigation here */}
